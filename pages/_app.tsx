d00d6a7ef5f276d9ebd8acb52de4b33260d577fb
Gabriel <gabrielperales@gmail.com>
2021-02-26 23:06:08 +0100

    added open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import 'tailwindcss/tailwind.css'
+import { DefaultSeo } from 'next-seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+		<>
+			<DefaultSeo
+				openGraph={{
+					type: 'website',
+					locale: 'en_EN',
+					url: 'https://clubhousetools.app/',
+					site_name: 'Clubhouse Tools',
+				}}
+				twitter={{
+					handle: '@g_perales',
+					cardType: 'summary_large_image',
+				}}
+			/>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import ImagePicker from '../components/ImagePicker'
 import Feedback from '../components/Feedback'
 
@@ -9,6 +10,23 @@ export default function Home() {
           <title>Clubhouse tools</title>
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛠</text></svg>"/>
         </Head>
+				<NextSeo
+					title="Clubhouse Tools"
+					description="Change your Clubhouse status with a badge in your image profile"
+					canonical="https://clubhousetools.app/"
+					openGraph={{
+						title: '🛠 Clubhouse Tools',
+						description: 'Change your Clubhouse status with a picture!',
+						images: [
+							{
+								url: 'https://clubhousetools.app/open-graph-image.jpg',
+								width: 800,
+								height: 600,
+								alt: 'Featured image of Clubhouse Tools',
+							}
+						],
+					}}
+				/>
 
         <h1 className="p-4 text-4xl font-bold text-gray-800">🛠 Clubhouse tools</h1>
         <article className="flex flex-col flex-grow mt-8 items-center">
